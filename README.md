d49e61f25400e4d83eb7e67ea7f51b88f0d32762
asnewman <ashznewman@gmail.com>
2021-01-10 10:26:26 -1000

    untested config update

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ Base code forked from [miguelgrinberg/flask-video-streaming](https://github.com/
 ## Running
 ### Prerequisites
 - Python 3.6+
-- pipenv
+- virtualenv
 ### Running
 1. Clone code
-1. `cd` into the directory and run `pipenv install`
+1. `cd` into the directory and start a new virtualenv
+1. `pip install -r requirements.txt`
 1. Update `.env` with the appropriate values
-1. `pipenv run python3 app.py`
+1. `python app.py`
--- camera_pi.py
+++ camera_pi.py
@@ -2,15 +2,17 @@ import io
 import time
 import datetime as dt
 import os
+import sys
 import picamera
+import json
 from base_camera import BaseCamera
 from emailer import Emailer
+import cv2
 
 class Camera(BaseCamera):
     @staticmethod
-    def frames():
+    def frames(self):
         with picamera.PiCamera() as camera:
-            #camera.resolution = (320, 240)
             camera.resolution = (480, 560)
             camera.annotate_text_size = 20 # (values 6 to 160, default is 32)
             camera.annotate_text = dt.datetime.now().strftime('%A %d %b %Y')
@@ -34,8 +36,15 @@ class Camera(BaseCamera):
 
             # Start streaming
             stream = io.BytesIO()
+            qr_code_check_counter = 0
             for _ in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
+                qr_code_check_counter += 1
+                if (qr_code_check_counter == 1000) {
+                    self.handle_qr_check(camera)
+                    qr_code_check_counter = 0
+                }
+
                 # return current frame
                 stream.seek(0)
                 yield stream.read()
@@ -43,3 +52,25 @@ class Camera(BaseCamera):
                 # reset stream for next frame
                 stream.seek(0)
                 stream.truncate()
+
+    def qr_decoder(image_location):
+        image = cv2.imread(image_location)
+        qr_code_detector = cv2.QRCodeDetector()
+        # will be none if no QR code is found
+        return qr_code_detector.detectAndDecode(image)
+    
+    def handle_qr_check(camera):
+        image_location = '/home/pi/Desktop/tmp.jpg'
+        camera.capture(image_location)
+        decoded_qr_code = self.qr_decoder(image_location)
+        
+        if (decoded_qr_code != "") {
+            new_data = json.load(decoded_qr_code)
+            with open('.env', 'w') as f:
+                env_vars = ["SEND_EMAIL", "SENDER_EMAIL", "SENDER_EMAIL_PASSWORD", "RECEPIENT_EMAIL"]
+                for var : env_vars:
+                    f.write(f'{var}={new_data[var] if new_data[var] != None else os.getenv(var)}\n')
+                f.close()
+
+            os.execv(__file__, sys.argv)
+        }
