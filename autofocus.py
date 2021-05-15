df86179eedecc9984486aa3e8d82b4d25b7f152b
asnewman <ashznewman@gmail.com>
2021-05-15 15:58:30 -0700

    standalone frame by frame

--- autofocus.py
+++ autofocus.py
@@ -1,5 +1,5 @@
 import cv2  # sudo apt-get install python-opencv
-import numpy as py
+import numpy as np
 import os
 import time
 import smbus
@@ -34,11 +34,9 @@ def laplacian(img):
     return cv2.mean(img_sobel)[0]
 
 
-def calculation(camera):
-    rawCapture = PiRGBArray(camera)
-    camera.capture(rawCapture, format="bgr", use_video_port=True)
-    image = rawCapture.array
-    rawCapture.truncate(0)
+def calculation(curr_frame):
+    bytes = np.asarray(bytearray(curr_frame), dtype=np.uint8)
+    image = cv2.imdecode(bytes, cv2.IMREAD_COLOR)
     return laplacian(image)
 
 def run_autofocus(camera):
@@ -79,3 +77,69 @@ def run_autofocus(camera):
     focusing(max_index)
 
     print("Camera focused")
+
+
+if __name__ == "__main__":
+    # open camera
+    camera = picamera.PiCamera()
+
+    # camera.awb_gains=4
+    # camera.exposure_mode='off'
+    # camera.awb_mode='fluorescent'
+    # open camera preview
+    camera.start_preview()
+    # set camera resolution to 640x480(Small resolution for faster speeds.)
+    camera.resolution = (640, 480)
+    time.sleep(0.1)
+    print("Start focusing")
+
+    max_index = 10
+    max_value = 0.0
+    last_value = 0.0
+    dec_count = 0
+    focal_distance = 10
+
+    for frame in camera.capture_continuous(stream, 'jpeg',
+                                           use_video_port=True):
+
+        # return current frame
+        stream.seek(0)
+        curr_frame = stream.read()
+
+        # Adjust focus
+        focusing(focal_distance)
+        # Take image and calculate image clarity
+        val = calculation(curr_frame)
+        # Find the maximum image clarity
+        if val > max_value:
+            max_index = focal_distance
+            max_value = val
+
+        # If the image clarity starts to decrease
+        if val < last_value:
+            dec_count += 1
+        else:
+            dec_count = 0
+        # Image clarity is reduced by six consecutive frames
+        if dec_count > 6:
+            break
+        last_value = val
+
+        # Increase the focal distance
+        focal_distance += 15
+        if focal_distance > 1000:
+            break
+
+    # Adjust focus to the best
+    focusing(max_index)
+    time.sleep(1)
+    # set camera resolution to 2592x1944
+    camera.resolution = (1920, 1080)
+    # save image to file.
+    camera.capture("test.jpg")
+    print("max index = %d,max value = %lf" % (max_index, max_value))
+    # while True:
+    #	time.sleep(1)
+
+    camera.stop_preview()
+    camera.close()
